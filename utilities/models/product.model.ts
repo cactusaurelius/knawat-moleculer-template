c950668e21ae5569533630753f7f3a638fcea20b
Hazem Khaled <hazem.khaled@gmail.com>
2020-12-29 18:20:26 +0300

    Schema paramas should extend from

--- utilities/models/product.model.ts
+++ utilities/models/product.model.ts
@@ -2,13 +2,13 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 import { Product } from '../types';
 
-const ProductSchema = new Schema<Product>({
+export interface ProductDocument extends Document<Product> {}
+
+const ProductSchema = new Schema<ProductDocument>({
   name: { type: String, required: true, unique: true },
   category: { type: String },
   price: { type: Number, required: true },
 });
-
-export interface ProductDocument extends Document<Product> {}
 
 export const ProductModel = mongoose.model<ProductDocument>(
   'Product',
